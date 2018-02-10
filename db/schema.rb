36b40e85f6d3612357f6ed5aa2706fe3074b7e09
alexrojas <alex.rojas@agintechllc.com>
2018-02-09 23:20:06 -0500

    added custom css, model restaurant, and updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180210015854) do
+ActiveRecord::Schema.define(version: 20180210033327) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
