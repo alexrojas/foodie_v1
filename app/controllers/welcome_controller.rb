ed7a9fb820a67e8b62dfff1282cd197dc4ea4b23
alexrojas <alex.rojas@agintechllc.com>
2018-02-18 18:42:21 -0500

    last commit API working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,9 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, :only => [ :index ]
   def index
-    @places = GoogleMapsService.nearby_places 33127
+    @places = GoogleMapsService.nearby_places 33131
     Rails.logger.info @places
   end
+
+
 end
